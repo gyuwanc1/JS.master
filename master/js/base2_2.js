95c23291b2930b53c8e9e2653ed977ea0833a572
b_gyuwan <gyuwanc@gmail.com>
2022-03-17 17:17:24 +0900

    [add] 2.3 datatype JS

--- master/js/base2_2.js
+++ master/js/base2_2.js
@@ -10,3 +10,30 @@ var y = 10;
 console.log(x);
 console.log(y);
 
+
+// let msg1; //기본적인 선언
+// //let x,y; // 복수의 변수 선언
+// let greeting = '안녕하세요, 자바스크립트'; //초기값을 설정
+
+// let msg= '뭐냐뭐냐';
+// let msg = '후후후'; 
+//let 은 변수선언의 중복을 허가하지 않음
+
+var msg='뭐냐뭐냐';
+var msg='후후후';
+
+console.log(msg);
+
+//var의 변수선언은 중복허용  마지막에 쓰인 값으로 덮어쓰인다.
+
+//상수선언
+
+var price = 100;
+console.log(price * 1.08);
+
+const Tax = 1.08;
+
+console.log(price * Tax);
+
+
+
