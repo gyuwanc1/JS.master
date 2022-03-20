6972542787052807dfaf0af4d47cfb71ddc1d7fb
b_gyuwan <gyuwanc@gmail.com>
2022-03-20 16:39:39 +0900

    [add] 2.4 oprator

--- master/js/base2_3.js
+++ master/js/base2_3.js
@@ -1,6 +1,6 @@
-
+{
 var x = '안녕하세요,자바스크립트'
-x = 100;
+
 
 console.log(x);
 
@@ -17,6 +17,7 @@ console.log(x);
 console.log('He\'s Hero!!'); //<== (\')를 단순히 (')로 인식
 
 // window.alert('안녕하세요,자바스크립트! \n열심히공부합시다! ')
+}
 
 {
   //template str
@@ -28,3 +29,34 @@ console.log('He\'s Hero!!'); //<== (\')를 단순히 (')로 인식
   console.log(str);
 
 }
+
+{
+  var data = ['자바스크립트', 'Ajax', 'asp,net'];
+  console.log (data[0]);
+  //배열명[인덱스번호]
+}
+
+{
+  var data= ['javascript', ['jQuery', 'prototype'],'asp.net'];
+  console.log(data[1][0]);
+  //배열명[인데스번호] [인덱스번호]
+  
+}
+
+{
+  var obj = { x:1, y:2, z:3};// {키명(프로퍼티명):값, 키명(프로퍼티명),값}
+  console.log(obj.x); //객체명.프로퍼티명
+  console.log(obj['x']); // 객체명['프로퍼티명']
+
+}
+
+{
+  var y;
+  var obj = {a:123};
+  console.log(y); //(값이 설정 되지 않음)
+  console.log(obj.b); // (프로퍼티가 존재하지 않는다)
+}
+
+//undefined -> 정의되지않았다, 본래부터 참조하려는 생각을 하지 않았다
+
+//null -> 비어있다.
