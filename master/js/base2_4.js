c216e540c64449f9a406da98b3bc7a2f71a9d0d4
b_gyuwan <gyuwanc@gmail.com>
2022-03-28 19:24:40 +0900

    [add]operator array

--- master/js/base2_4.js
+++ master/js/base2_4.js
@@ -48,3 +48,61 @@ console.log (1234+today);
   console.log(data2);
 }
 //변수는 메모리 그자체 번호를 부여한 주소를 가지고 있음. 
+
+//상수 
+{
+  const tax = 1.08;
+  //tax = 1.1; //오류
+  
+} // 기본형은  값의 재대입시 오류
+
+{
+  const data = [1,2,3];
+  //data = [4,5,6]; <- 오류 -1
+  data[1] = 10; // -2
+  console.log(data);
+
+  // 1번은 배열 자체를 제대입하기에 불가하여 오류지만 2번은 내용만 변경하고있기에 상수위반은 아님. 
+}
+
+//분할대입(배열)
+{
+  var data= [56,40,26,82,19,17,73,99];
+  var x0 = data[0]
+  var x1 = data[1]
+  var x2 = data[2]
+  var x3 = data[3]
+  var x4 = data[4]
+  var x5 = data[5]
+  var x6 = data[6]
+  var x7 = data[7]
+
+  console.log(x0) //56
+}
+
+{
+  let data = [56,40,26,82,19,17,73,99];
+  // let [x0,x1,x2,x3,x4,x5,x6,x7] = data;
+  // console.log(x0);
+  // console.log(x1);
+  // console.log(x2);
+  // console.log(x3);
+  // console.log(x4);
+  // console.log(x5);
+  // console.log(x6);
+  // console.log(x7);
+  let[x0,x1,x2,...other] = data;
+  console.log(x0);
+  console.log(x1);
+  console.log(x2);
+  console.log(other);
+
+}
+
+{
+  let x = 1;
+  let y = 2;
+  [x,y] = [y,x]
+  console.log(x,y);
+}
+
