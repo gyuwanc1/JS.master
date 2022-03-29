747f47e0a01cc8adabe83b70e49fa812e7b5bb61
b_gyuwan <gyuwanc@gmail.com>
2022-03-29 16:57:06 +0900

    [add]object publish

--- master/js/base2_4.js
+++ master/js/base2_4.js
@@ -106,3 +106,16 @@ console.log (1234+today);
   console.log(x,y);
 }
 
+{
+  let book = { title:'java포켓 레퍼런스', publish:'기술평론사', price:26800 };  
+  let {price, title, memo = '없음' } = book;
+  console.log(title);
+  console.log(price);  
+  console.log(memo);
+}
+
+//객체분해
+{
+  
+}
+
