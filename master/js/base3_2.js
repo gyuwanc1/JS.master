1a248e27c9f4b8e7cd31eaa75cc275da828e8cfb
b_gyuwan <gyuwanc@gmail.com>
2022-07-28 12:16:30 +0900

    Mathod : Math

---
+++ master/js/base3_2.js
@@ -0,0 +1,13 @@
+// JS. MATH 
+
+console.log(Math.abs(-100)); //100
+console.log(Math.clz32(1)); //31
+console.log(Math.min(20, 40, 60)); //20
+console.log(Math.max(20, 40, 60)); //60
+console.log(Math.pow(5, 3)); //125 거듭제곱
+console.log(Math.random()); //임의의 값
+console.log(Math.sign(-100)); //지정한 값이 양수이면1, 음수이면 -1, 0이면 0
+console.log(Math.ceil(1234.56)); //1235 소수점 이하 올림
+console.log(Math.floor(1234.56)) //1234 소수점 이하 버림
+console.log(Math.round(1234.56)); //1235 반올림
+console.log(Math.trunc(1234.56)); //1234 버림
