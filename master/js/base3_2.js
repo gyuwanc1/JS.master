377d3514bc8e076f7cf76517c3e1d1a7acc88723
b_gyuwan <gyuwanc@gmail.com>
2022-07-29 12:49:46 +0900

    Methode: Math2

--- master/js/base3_2.js
+++ master/js/base3_2.js
@@ -11,3 +11,12 @@ console.log(Math.ceil(1234.56)); //1235 소수점 이하 올림
 console.log(Math.floor(1234.56)) //1234 소수점 이하 버림
 console.log(Math.round(1234.56)); //1235 반올림
 console.log(Math.trunc(1234.56)); //1234 버림
+
+console.log(Math.sqrt(81)); //9  제곱근
+console.log(Math.cbrt(81)) //세제곱근
+console.log(Math.hypot(3,4)) //인수의 제곱합의 제곱근
+
+
+//Math는 인스턴스화 할수 없다
+//Math.프로퍼티명
+//Math.메소드명(인수)
