ab50d2b0b0d33b5ff0797e83e8f9fc008d2fc56e
b_gyuwan <gyuwanc@gmail.com>
2022-07-31 12:21:40 +0900

    Array/Map/set

---
+++ master/js/base3_3.js
@@ -0,0 +1,14 @@
+//배열 조작하기 Array객체
+
+{
+  var ary = ['베트맨', '슈퍼맨', '아쿠아맨'];
+  console.log(ary);
+}
+
+{
+  var ary = new Array('배트맨','슈퍼맨','아쿠아맨'); //지정요소로 배열을 생성
+  var ary = new Array(); //빈배열을 생성
+  var ary = new Array(10); //지정사이즈(인데스 0~9)로 빈배열 생성
+  var ary = [];
+  console.log(ary);
+}
