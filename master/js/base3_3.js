d49060b0f616b6a0e7eda1a51b9319f8c737a922
b_gyuwan <gyuwanc@gmail.com>
2022-08-02 14:13:23 +0900

    Array:: object member

--- master/js/base3_3.js
+++ master/js/base3_3.js
@@ -12,3 +12,19 @@
   var ary = [];
   console.log(ary);
 }
+
+//Array 객체의 멤버
+
+{
+  var ary1 = ['sato', 'takae', 'osada', 'hio', 'saitoh', 'sato'];
+  var ary2 = ['yabuki', 'aoki', 'moriyama', 'yamada'];
+
+  console.log(ary1.length); //6
+  console.log(Array.isArray(ary1)); //true 지정한 객체가 배열인가?
+  console.log(ary1.toString()); //요소의 형식으로 문자전환
+  console.log(ary1.indexOf('sato')); //일치하는 첫번째 요소 
+  console.log(ary1.lastIndexOf('sato')); // 일치하는 마지막요소
+  console.log(ary1.concat(ary2)); //배열 연결
+  console.log(ary1.join('/')); // 배열내의 요소를 /로 연결
+  console.log(ary1.slice(1)); //start+1~end
+}
